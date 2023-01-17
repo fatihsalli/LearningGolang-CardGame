7b51ee84a3987e6cc00d04fd2e9f574faae5c87c
Fatih Şallı <mimsallifatih@gmail.com>
2023-01-17 22:08:41 +0300

    cardgame

--- deck.go
+++ deck.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -10,12 +13,13 @@ import (
 // Different from OOM we are creating new type which is inherited from [] string - actually equal
 type deck []string
 
+// This function gives us deck value as []string with mixing cardSuits and cardValues
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	// If we don't declare 'i' value we can say that with '_' - otherwise we will get error
+	// If we don't want to declare 'i' value we can say that with '_' | Otherwise we will get error
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
@@ -32,12 +36,46 @@ func (d deck) print() {
 	}
 }
 
-// => Return two deck - one of them for handCard and the other for remain
+// => Return two deck - one of them for handCard and the other for remainCard
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-// => To create []byte we need string rather than []string.
+// => To create []byte we need string. We can't convert from []string
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+// => To save file (Package has changed from 'ioutil' to 'os')
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// => To read from file
+// 'nil' value with go using like null or no value
+func newDeckFromFile(filename string) deck {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - log the error and entirely quit the program (Chosen)
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	// string(data) => Ace of Spades,Two of Spades,Three of Spades ...
+	s := strings.Split(string(data), ",")
+	return deck(s)
+}
+
+// => To mix card (To Shuffle)
+func (d deck) shuffle() {
+	// Every time we need to new combination of number, so we have to change source to create different rand numbers
+	// We use time.Now().UnixNano() to create int-64 value.
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		//For swap or change two values
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -1,56 +1,44 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	/*
-		cards := newDeck()
-
-		//Deal method give two array first of this for hand and second one for remaining Cards
-		hand, remainingCards := deal(cards, 5)
-
-		hand.print()
-		remainingCards.print()
-		//=> []byte [72 105 32 116 104 101 114 101 33] Ascii code of "Hi there!"
-		greeting := "Hi there!"
-		fmt.Println([]byte(greeting))
-
-	*/
-
-	//=> To write hand and remain cards, we used to 'io-util' library with func WriteFile(...)
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.shuffle()
+	cards.print()
 
-	hand, remainingCards := deal(cards, 5)
-	fmt.Println(hand)
-	fmt.Println(remainingCards)
 }
 
 /*
 func main() {
+	// => Define variable
 	var card string = "Ace of Spades" => Alternative
 	card := newCard()
-	card = "Five of Diamonds" => We can change value
+	card = "Five of Diamonds" => We can change value, but we can't change type
 
-	=>Slice - To create an array if its count can grow or shrink
+	// => Slice - To create an array if its count can be grown or shrink
 	After create new type deck we changed deck instead of [] string
 	cards := deck{"Ace of Diamonds", newCard()}
 
-	=> To add new member of array
+	// => To add new member of array
 	cards = append(cards, "Six of Spades")
 
-	=>For loops => for index,card:=range cards{...}
+	// => For loops => for index,card:=range cards{...}
 	index:index card: current card we're iterating over - range cards: take the slice of 'cards' and loop over it
 	for i, card := range cards {
 		fmt.Println(i, card)
 	}
 
-}
+	// => To create deck ([]string)
+	cards := newDeck()
 
-func => function - newCard() => Define action with method name - string => return value of type
-func newCard() string {
-	return "Five of Diamonds"
+	// => Deal method give two array first of this for handCard and second one for remainCard
+	hand, remainingCards := deal(cards, 5)
+
+	// => Print method shows on the terminal
+	hand.print()
+	remainingCards.print()
+
+	// => []byte [72 105 32 116 104 101 114 101 33] Ascii code of "Hi there!"
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
 */
